75c57c69762472fe237fc4034738460cbcfb7d7b
priyanka-ck <priyanka.gorai@cloudkaptan.com>
2025-01-14 15:20:50 +0530

    created base and reusable class

---
+++ src/main/java/com/base/BaseClass.java
@@ -0,0 +1,28 @@
+package com.base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BaseClass {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    
+}
---
+++ src/main/java/com/utils/ReusableClass.java
@@ -0,0 +1,47 @@
+package com.utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import com.base.BaseClass;
+
+public class ReusableClass extends BaseClass {
+    public void navigateTo(String url) {
+        driver.get(url);
+    }
+
+    public WebElement waitForElementVisible(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public WebElement waitForElementClickable(By locator) {
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    public void click(By locator) {
+        waitForElementClickable(locator).click();
+    }
+
+    public void sendKeys(By locator, String text) {
+        waitForElementVisible(locator).sendKeys(text);
+    }
+
+    public String getText(By locator) {
+        return waitForElementVisible(locator).getText();
+    }
+
+    public boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (org.openqa.selenium.NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    public void waitForPageLoad() {
+        wait.until(webDriver -> ((org.openqa.selenium.JavascriptExecutor) webDriver)
+                .executeScript("return document.readyState").equals("complete"));
+    }
+    
+}
